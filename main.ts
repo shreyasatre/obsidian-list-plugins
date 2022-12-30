a0930d855ce3fd7723806f5a48d45f5e6996974b
Shreyas <shreyas.atre@gmail.com>
2022-12-30 00:30:22 -0500

    adding required functions and interfaces

--- main.ts
+++ main.ts
@@ -1,54 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, normalizePath, FileSystemAdapter } from 'obsidian';
+import * as fs from 'fs';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ListPluginsSettings {
+	listPluginsNoteName: string;
+	listPluginsNotePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ListPluginsSettings = {
+	listPluginsNoteName: '',
+	listPluginsNotePath: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface manifestPluginDetails {
+	id: string;
+    name: string;
+    version: string;
+    minAppVersion: string;
+    description: string;
+    author: string;
+    authorUrl: string;
+    isDesktopOnly: boolean;
+}
+
+interface communityPluginDetails {
+	id: string;
+	name: string;
+	author: string;
+	branch: string;
+	description: string;
+	repo: string;
+}
+
+export default class ListPluginsPlugin extends Plugin {
+	settings: ListPluginsSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('toy-brick', 'List Plugins: generate list', async (evt: MouseEvent) => {
+
+			this.makeListFile();
+
 		});
+		
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'list-plugins-generate-list',
+			name: 'List Plugins: generate list',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -56,7 +58,8 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						console.log("command can be run");
+						this.makeListFile();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +69,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new ListPluginsSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -79,7 +82,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {
-
+		// Nothing.
 	}
 
 	async loadSettings() {
@@ -89,28 +92,120 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	/**
+	 * @summary Generates the file containing the list of user's plugins.
+	 */
+	async makeListFile() {
+		
+		const communityPluginsUrl = "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json";
+
+		let fileName = "";
+		let filePath = "";
+		let fileContents = "";
+
+		let allPlugins: communityPluginDetails[] = [];
+		let listOfPluginManifests: manifestPluginDetails[];
+		let installedPluginsCount = 0;
+
+		if(this.settings.listPluginsNoteName.trim() == "") {
+			fileName = "List of Plugins - " + formatDate(new Date());
+		} else {
+			fileName = normalizePath(this.settings.listPluginsNoteName);
+		}
+
+		filePath = normalizePath(this.settings.listPluginsNotePath + "/" + fileName + ".md");
+
+		if (await this.app.vault.adapter.exists(filePath)) {
+			new Notice(`${fileName} already exists!`);
+		} else {
+	
+			fetch(communityPluginsUrl)
+				.then(res => res.json())
+				.then((out) => {
+
+					allPlugins = out;
+
+					const pluginsDirPath = this.getAbsolutePath();
+					const installedPlugins = fs.readdirSync(pluginsDirPath);
+
+					listOfPluginManifests = [];
+					installedPlugins.forEach(pluginManifest => {
+						const manifestFile = pluginsDirPath + "/" + pluginManifest + "/manifest.json";
+						const stats = fs.statSync(manifestFile);
+						if(stats.isFile()) {
+							const rawData = fs.readFileSync(manifestFile);
+							const pluginData = JSON.parse(rawData.toString());
+							listOfPluginManifests.push(pluginData);
+						}
+					});
+
+					listOfPluginManifests.forEach(pluginItem => {
+						
+						const found = allPlugins.find(matchingPlugin => matchingPlugin.id == pluginItem.id);
+						
+						if(found !== undefined) {
+							
+							if(fileContents.trim() == "") {
+								fileContents = "name | author | open in obsidian " + "\n"
+								+ "---|---|---" + "\n";
+							}
+							
+							// "obsidian://show-plugin?id="+encodeURIComponent(h)
+							// https://github.com/
+							fileContents += "[" + found.name + "](https://github.com/" + found.repo + ") | "
+											+ found.author + " | "
+											+ "[get](obsidian://show-plugin?id=" + encodeURIComponent(found.id) + ")\n";
+
+							installedPluginsCount++;
+
+						}
+					});
+
+					const metaData = "## List of installed plugins\n" +
+										"Curated: " + formatDate(new Date()) + "\n" +
+										"Total: " + installedPluginsCount + "\n\n" +
+										"---\n\n";
+
+					fileContents = metaData + fileContents;
+					this.app.vault.create(filePath, fileContents);
+					new Notice(`${fileName} has been created`);
+
+				})
+				.catch(err => { throw err });
+
+		}
+
+	}
+
+	/**
+	 * @summary Gets the absolute path of the plugins folder in the vault.
+	 * @returns Absolute path of the plugins folder in the vault.
+	 */
+	getAbsolutePath(): string {
+        
+		let basePath;
+
+		// base path
+        if (this.app.vault.adapter instanceof FileSystemAdapter) {
+            basePath = this.app.vault.adapter.getBasePath();
+        } else {
+            throw new Error('Cannot determine base path.');
+        }
+
+        // relative path
+        const relativePath = `${this.app.vault.configDir}/plugins`;
+        
+		// absolute path
+        return `${basePath}/${relativePath}`;
+    }
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ListPluginsSettingTab extends PluginSettingTab {
+	plugin: ListPluginsPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ListPluginsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -119,19 +214,52 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'List Plugins Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Note name')
+			.setDesc('The name of the note to store the list of plugins. If it doesn\'t exist, a new note will be created.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('name')
+				.setValue(this.plugin.settings.listPluginsNoteName)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('List plugins note name: ' + value);
+					this.plugin.settings.listPluginsNoteName = value;
 					await this.plugin.saveSettings();
 				}));
-	}
+
+		new Setting(containerEl)
+			.setName('Note path')
+			.setDesc('Path of the note.')
+			.addText(text => text
+				.setPlaceholder('folder')
+				.setValue(this.plugin.settings.listPluginsNotePath)
+				.onChange(async (value) => {
+					console.log('List plugins note path: ' + value);
+					this.plugin.settings.listPluginsNotePath = value;
+					await this.plugin.saveSettings();
+				}));
+		}
 }
+
+function padTo2Digits(num: number) {
+	return num.toString().padStart(2, '0');
+}
+
+function formatDate(date: Date) {
+
+    // YYYYMMDD_hhmmss
+    return (
+	[
+		date.getFullYear(),
+		padTo2Digits(date.getMonth() + 1),
+		padTo2Digits(date.getDate()),
+	].join('') +
+	'_' +
+	[
+		padTo2Digits(date.getHours()),
+		padTo2Digits(date.getMinutes()),
+		padTo2Digits(date.getSeconds()),
+	].join('')
+	);
+  }
